edb779ba19aef6e3d5de14866dedee3056205cae
aman <amanisyoyo@gmail.com>
2021-04-06 21:04:51 +0530

    this is my second program

---
+++ stringtochar.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class stringtochar {
+    public static void main(String args[]) {
+        System.out.println("enter the string");
+        Scanner sc= new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(""+str);
+    }
+
+    
+}

1dd2cd26a288adbfc131a6c704104004a965da77
aman <amanisyoyo@gmail.com>
2021-04-06 21:02:52 +0530

    this is my first program

---
+++ typeconversion.java
@@ -0,0 +1,11 @@
+public class typeconversion {
+    public static void main (String args[]){
+        byte b;
+        int i=257;
+        double d=323.142;
+        
+        System.out.println("\n Conversion if double to byte");
+        b=(byte) i;
+        System.out.println("i and b "+ i +" "+ b);
+    }
+}
